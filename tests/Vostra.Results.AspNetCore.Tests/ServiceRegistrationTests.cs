5becf4e8865ee8400f084e8961242bf00c38860d
rcn123 <excelligent@protonmail.com>
2026-06-20 21:02:07 +0200

    feat(aspnetcore): add AddVostraResults DI registration

---
+++ tests/Vostra.Results.AspNetCore.Tests/ServiceRegistrationTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Vostra.Results.AspNetCore.Tests;
+
+public class ServiceRegistrationTests
+{
+    [Fact]
+    public void AddVostraResults_registers_configured_options()
+    {
+        var provider = new ServiceCollection()
+            .AddVostraResults(o => o.MapStatus(ErrorType.Conflict, 422))
+            .BuildServiceProvider();
+
+        var options = provider.GetRequiredService<VostraResultsOptions>();
+        ErrorStatusResolver.Resolve(new ConflictError("x"), options).Should().Be(422);
+    }
+
+    [Fact]
+    public void AddVostraResults_without_configure_registers_default_behavior()
+    {
+        var provider = new ServiceCollection().AddVostraResults().BuildServiceProvider();
+
+        var options = provider.GetRequiredService<VostraResultsOptions>();
+        ErrorStatusResolver.Resolve(new NotFoundError("x"), options).Should().Be(404);
+    }
+}
