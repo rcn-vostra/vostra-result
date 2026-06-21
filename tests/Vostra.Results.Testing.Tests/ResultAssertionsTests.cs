f4e3ecaa1ea8d9fd21690d4da90951f1caa48cd5
rcn123 <excelligent@protonmail.com>
2026-06-21 10:41:39 +0200

    feat(testing): add VostraAssertionException and fluent sync assertions

---
+++ tests/Vostra.Results.Testing.Tests/ResultAssertionsTests.cs
@@ -0,0 +1,95 @@
+namespace Vostra.Results.Testing.Tests;
+
+public class ResultAssertionsTests
+{
+    [Fact]
+    public void ShouldBeSuccess_returns_value_on_success()
+    {
+        Result<int> result = 42;
+
+        var value = result.ShouldBeSuccess();
+
+        value.Should().Be(42);
+    }
+
+    [Fact]
+    public void ShouldBeSuccess_throws_on_failure()
+    {
+        Result<int> result = new NotFoundError("nope", "X.NotFound");
+
+        var act = () => result.ShouldBeSuccess();
+
+        act.Should().Throw<VostraAssertionException>().WithMessage("*X.NotFound*");
+    }
+
+    [Fact]
+    public void ShouldHaveError_by_code_passes_when_any_error_matches()
+    {
+        Result<int> result = new ErrorBase[] { new ValidationError("a", "F.A"), new ValidationError("b", "F.B") };
+
+        var chained = result.ShouldHaveError("F.B");
+
+        chained.Should().Be(result);
+    }
+
+    [Fact]
+    public void ShouldHaveError_by_code_throws_when_no_error_matches()
+    {
+        Result<int> result = new NotFoundError("nope", "X.NotFound");
+
+        var act = () => result.ShouldHaveError("Other.Code");
+
+        act.Should().Throw<VostraAssertionException>();
+    }
+
+    [Fact]
+    public void ShouldHaveError_by_type_passes()
+    {
+        Result<int> result = new NotFoundError("nope", "X.NotFound");
+
+        result.ShouldHaveError(ErrorType.NotFound);
+    }
+
+    [Fact]
+    public void ShouldBeNotFound_sugar_passes()
+    {
+        Result<int> result = new NotFoundError("nope", "X.NotFound");
+
+        result.ShouldBeNotFound();
+    }
+
+    [Fact]
+    public void Assert_runs_action_on_success_and_returns_result()
+    {
+        Result<int> result = 10;
+        var seen = 0;
+
+        var chained = result.Assert(v => seen = v);
+
+        seen.Should().Be(10);
+        chained.Should().Be(result);
+    }
+
+    [Fact]
+    public void Assert_throws_when_result_is_failure()
+    {
+        Result<int> result = new NotFoundError("nope", "X.NotFound");
+
+        var act = () => result.Assert(_ => { });
+
+        act.Should().Throw<VostraAssertionException>();
+    }
+
+    [Fact]
+    public void Failure_message_includes_request_context_when_present()
+    {
+        var error = new NotFoundError("gone", "X.NotFound")
+            .WithMetadata(new Dictionary<string, object?> { ["request"] = new RequestContext("GET", "products/9", null) });
+        Result<int> result = (ErrorBase)error;
+
+        var act = () => result.ShouldBeSuccess();
+
+        act.Should().Throw<VostraAssertionException>()
+            .WithMessage("*GET*products/9*");
+    }
+}
